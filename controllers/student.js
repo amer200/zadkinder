b9778f167a12a109b4e5afeb30a009c3b275497b
amer200 <amermostaafa@gmail.com>
2023-03-16 17:42:23 +0200

    commit

--- controllers/student.js
+++ controllers/student.js
@@ -52,8 +52,67 @@ exports.getStudents = async (req, res) => {
     } catch {
         console.log(err)
         res.status(500).json({
-            msg: err
+            msg: err.message
         })
     }
 
+}
+exports.removeStudent = (req, res) => {
+    const id = req.params.id;
+    Student.findByIdAndRemove(id)
+        .then(s => {
+            res.status(200).json({
+                msg: "ok"
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                msg: err.message
+            })
+        })
+}
+exports.editStudent = (req, res) => {
+    const name = req.body.name;
+    const gender = req.body.gender;
+    const nationality = req.body.nationality;
+    const cid = req.body.cid;
+    const kinder = req.body.kinder;
+    const time = req.body.time;
+    const fathername = req.body.fathername;
+    const fatherno = req.body.fatherno;
+    const motherno = req.body.motherno;
+    const address = req.body.address;
+    const bus = req.body.bus;
+    const id = req.params.id;
+    Student.findById(id)
+        .then(s => {
+            if (s) {
+                s.name = name;
+                s.gender = gender;
+                s.nationality = nationality;
+                s.cid = cid;
+                s.kinder = kinder;
+                s.time = time;
+                s.fathername = fathername;
+                s.fatherno = fatherno;
+                s.motherno = motherno;
+                s.address = address;
+                s.bus = bus;
+                s.save()
+                    .then(s => {
+                        res.status(200).json({
+                            student: s
+                        })
+                    })
+            } else {
+                res.status(404).json({
+                    msg: "student not found"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                msg: err.message
+            })
+        })
 }
