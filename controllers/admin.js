c6fe3007e31880783bf7b27f88a3ad000861c275
Soha <sohaali710@gmail.com>
2023-08-10 21:48:40 +0300

    commit

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,48 +1,48 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt');
-const asyncHandler = require('express-async-handler')
 const Admin = require("../models/admin");
 
-exports.logIn = asyncHandler(async (req, res) => {
+exports.logIn = async (req, res) => {
     const { email, password } = req.body;
     // const a = await bcrypt.hash("123", 8)
     // console.log(a)
 
-    // try {
-    const admin = await Admin.findOne({ emai })
+    try {
+        const admin = await Admin.findOne({ email })
+        console.log(admin.password)
 
-    if (admin) {
-        req.session.admin = true
-        // const isMatch = await bcrypt.compareSync(password, admin.password)
-        // if (isMatch) {
-        if (password == admin.password) {
-            const user = {
-                id: "123",
-                name: "admin",
-                role: "admin"
+        if (admin) {
+            req.session.admin = true
+            // const isMatch = await bcrypt.compareSync(password, admin.password)
+            // if (isMatch) {
+            if (password == admin.password) {
+                const user = {
+                    id: "123",
+                    name: "admin",
+                    role: "admin"
+                }
+                const token = jwt.sign({ user: user }, process.env.ACCESS_TOKEN);
+                res.status(200).json({
+                    msg: "ok",
+                    token: token
+                })
+            } else {
+                res.status(400).json({
+                    msg: "wrong password or email"
+                })
             }
-            const token = jwt.sign({ user: user }, process.env.ACCESS_TOKEN);
-            res.status(200).json({
-                msg: "ok",
-                token: token
-            })
         } else {
             res.status(400).json({
                 msg: "wrong password or email"
             })
         }
-    } else {
-        res.status(400).json({
-            msg: "wrong password or email"
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            msg: "Server error"
         })
     }
-    // } catch (err) {
-    // console.log(err)
-    // res.status(500).json({
-    //     msg: "Server error"
-    // })
-    // }
-})
+}
 
 exports.changePassword = async (req, res) => {
     const { email, password, newPassword } = req.body;
--- middlewares/admin.js
+++ middlewares/admin.js
@@ -3,14 +3,16 @@ const jwt = require("jsonwebtoken");
 exports.isAdmin = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
+
     if (!token) {
-        return res.status(400).json({
-            msg: "token is requires"
+        res.status(400).json({
+            msg: "token is required"
         })
     }
+
     jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
         if (err) {
-            return res.status(400).json({
+            res.status(400).json({
                 msg: err
             })
         }
